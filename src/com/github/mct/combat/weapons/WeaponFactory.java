790d51ac1a9f6d59f3194d980ebae169753681d8
EDStaley <eds0017@uah.edu>
2020-03-07 14:13:22 -0600

    Finished weapons.

--- src/com/github/mct/combat/weapons/WeaponFactory.java
+++ src/com/github/mct/combat/weapons/WeaponFactory.java
@@ -1,30 +1,17 @@
 package com.github.mct.combat.weapons;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
+import java.util.*;
 
+/**
+ * Class that creates a weapon of the given archetype
+ *
+ * @author Elizabeth Staley
+ */
 public class WeaponFactory {
     /**
      * Map that relates the weapon archetype to array list of weapon names
      */
-    private Map<WeaponArchetype, List<String>> weaponClosest;
-
-    /**
-     * Array list for short weapons
-     */
-    private List<String> shortWeap;
-
-    /**
-     * Array list for medium weapons
-     */
-    private List<String> medWeap;
-
-    /**
-     * Array list for long weapons
-     */
-    private List<String> longWeap;
+    private Map<WeaponArchetype, ArrayList<String>> weaponClosest;
 
     /**
      * Map that relates the weapon name to the weapon attack rating
@@ -42,37 +29,53 @@ public class WeaponFactory {
      */
     public WeaponFactory()
     {
-
         //Create Weapon Closet
         weaponClosest = new HashMap<>();
         //Short weapons
-        shortWeap = new ArrayList<>();
-        weaponClosest.put(WeaponArchetype.SHORT, shortWeap);
+        weaponClosest.put(WeaponArchetype.SHORT, new ArrayList<>());
         weaponClosest.get(WeaponArchetype.SHORT).add("Dagger");
         weaponClosest.get(WeaponArchetype.SHORT).add("Cestus");
         weaponClosest.get(WeaponArchetype.SHORT).add("Gladius");
         //Medium weapons
-        medWeap = new ArrayList<>();
-        weaponClosest.put(WeaponArchetype.MEDIUM, medWeap);
+        weaponClosest.put(WeaponArchetype.MEDIUM, new ArrayList<>());
         weaponClosest.get(WeaponArchetype.MEDIUM).add("Staff");
         weaponClosest.get(WeaponArchetype.MEDIUM).add("Hand-and-a-half sword");
         weaponClosest.get(WeaponArchetype.MEDIUM).add("Rapier");
         //Long weapons
-        longWeap = new ArrayList<>();
-        weaponClosest.put(WeaponArchetype.LONG, longWeap);
+        weaponClosest.put(WeaponArchetype.LONG, new ArrayList<>());
         weaponClosest.get(WeaponArchetype.LONG).add("Halberd");
         weaponClosest.get(WeaponArchetype.LONG).add("Lance");
         weaponClosest.get(WeaponArchetype.LONG).add("Two-handed sword");
 
         //Create the attack ratings
         atkRatings = new HashMap<>();
+        //Short weapons
+        atkRatings.put("Dagger", 4);
+        atkRatings.put("Cestus", 5);
+        atkRatings.put("Gladius", 3);
+        //Medium weapons
+        atkRatings.put("Staff", 1);
+        atkRatings.put("Hand-and-a-half sword", 3);
+        atkRatings.put("Rapier", 3);
         //Long weapons
         atkRatings.put("Halberd", 3);
         atkRatings.put("Lance", 1);
         atkRatings.put("Two-handed sword", 2);
 
         //Create the defense ratings
-
+        defRatings = new HashMap<>();
+        //Short weapons
+        defRatings.put("Dagger", 1);
+        defRatings.put("Cestus", 0);
+        defRatings.put("Gladius", 3);
+        //Medium weapons
+        defRatings.put("Staff", 3);
+        defRatings.put("Hand-and-a-half sword", 2);
+        defRatings.put("Rapier", 1);
+        //Long weapons
+        defRatings.put("Halberd", 0);
+        defRatings.put("Lance", 2);
+        defRatings.put("Two-handed sword", 2);
     }
 
     /**
@@ -81,13 +84,24 @@ public class WeaponFactory {
      * (No unparameterized because a weapon must be provided values)
      *
      * @param arch The weapon archetype
-     * //@return Returns a weapon of the given archetype and initialized attack and defense ratings
+     * @return Returns a weapon of the given archetype and initialized attack and defense ratings
      */
-    public /*Weapon*/void MakeWeapon(WeaponArchetype arch)
+    public Weapon MakeWeapon(WeaponArchetype arch)
     {
-        //print all weapons of that type
+        //Get array list of all weapons that satisfy that archetype
+        ArrayList<String> temp = new ArrayList<>();
         if(weaponClosest.containsKey(arch)) {
-            System.out.println(weaponClosest.get(arch)); //add .get(i) to access specific member
+            temp = weaponClosest.get(arch);
         }
+
+        //select random weapon from the given archetype
+        Random rand = new Random();
+        String weaponName = temp.get(rand.nextInt(temp.size()));
+
+        //Create weapon
+        int aR = atkRatings.get(weaponName);
+        int dR = defRatings.get(weaponName);
+
+        return new Weapon(arch, aR, dR);
     }
 }
